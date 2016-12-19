ba2985483d0a6c46425f8790837568b05e0f5a96
Viveckh <vpandey@ramapo.edu>
2016-12-18 21:27:44 -0500

    Search results successfully displayed, jade templates divided

--- public/javascripts/viewManager.js
+++ public/javascripts/viewManager.js
@@ -3,7 +3,13 @@ $(window).load(function() {
     $('.thumbnail').matchHeight();
 });
 
+// Do something on HTML Change
+$(document).bind("DOMSubtreeModified", function() {
+    //console.log("New code generated");
+});
+
 /*
+// Detect key presses throughout the page
 $(document).keypress(function(e) {
     console.log(e.which);
 });
@@ -15,6 +21,23 @@ $(document).ready(function() {
         //If user presses the Enter Key
         if (e.which == 13) {
             console.log("Enter pressed");
+            var searchFor = $('#custom-searchBarText').val();
+
+            // Make ajax call to update the view with search results
+            $.ajax({
+                url: '/searchresults',
+                data: {searchFor: searchFor}
+            })
+                .done(function (data) {
+                    $('#custom-shelves').html(data);
+                    $('.thumbnail').matchHeight();
+                    //console.log(data);
+                });
+            /*
+            $.get('/searchresults', {searchFor: searchFor}, function() {
+                console.log("Getting search results");
+            });
+            */
         }
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,22 @@ router.get('/', function(req, res, next) {
         //console.log(customers);
         //console.log(data['100000001'].firstName);
         dbRequests.getMatchingItemsInfoForThumbnail('', function (items) { //Search for an empty string so that all the available books can be seen
-            console.log(items);
+            //console.log(items);
             res.render('index', { customers: customers, items : items });
         });
     });
 });
+
+/* GET search results */
+router.get('/searchresults', function(req, res, next) {
+    var searchFor = req.query.searchFor;
+    console.log("Searching in DB for: " + searchFor);
+    
+    dbRequests.getMatchingItemsInfoForThumbnail(searchFor, function (items) { //Search for an empty string so that all the available books can be seen
+        //console.log(items);
+        res.render('shelves', { items : items });
+    });
+});
+
 
 module.exports = router;
